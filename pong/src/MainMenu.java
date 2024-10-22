20c37696553345af67f2a87e41575dddfd261bf6
SylvioLabriola <sylvioez@gmail.com>
2024-10-22 17:56:10 -0300

    Update: changed the magic number of the size of text to a constant

--- pong/src/MainMenu.java
+++ pong/src/MainMenu.java
@@ -1,4 +1,93 @@
-package PACKAGE_NAME;
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
-public class MainMenu {
+public class MainMenu extends JFrame implements Runnable {
+
+    public Graphics2D g2;
+    public KL keyListener = new KL();
+    public ML mouseListener = new ML();
+    public Text startGame, exitGame, pong;
+    public boolean isRunning = true;
+
+
+    public MainMenu() {
+        this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+        this.setTitle(Constants.SCREEN_TITLE);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
+        this.addMouseListener(mouseListener);
+        this.addMouseMotionListener(mouseListener);
+        this.startGame = new Text("Start Game",new Font("Silkscreen",Font.PLAIN,40), Constants.SCREEN_WIDTH / 2.0 - 180.0, Constants.SCREEN_HEIGHT / 2.0, Color.WHITE);
+        this.exitGame = new Text("Exit", new Font("Silkscreen", Font.PLAIN,40), Constants.SCREEN_WIDTH / 2.0 - 80.0, Constants.SCREEN_HEIGHT / 2.0 + 60, Color.WHITE);
+        this.pong = new Text("Pong, Ping Pong!", new Font("Silkscreen", Font.PLAIN, Constants.TITLE_TEXT_SIZE), Constants.SCREEN_WIDTH / 2.0 - 380.0, 200, Color.WHITE);
+
+        g2 = (Graphics2D)getGraphics();
+    }
+
+    public void update(double dt) {
+        Image dbImage = createImage(getWidth(), getHeight());
+        Graphics dbg = dbImage.getGraphics();
+        this.draw(dbg);
+        g2 = (Graphics2D) this.getGraphics();
+        g2.drawImage(dbImage, 0, 0, this);
+
+        if(mouseListener.getMouseX() > startGame.x && mouseListener.getMouseX() < startGame.x + startGame.width &&
+                mouseListener.getMouseY() > startGame.y - startGame.height / 2.0 && mouseListener.getMouseY() < startGame.y + startGame.height / 2.0){
+            startGame.color = new Color(158,158,158);
+
+            if(mouseListener.isMousePressed()){
+                Main.changeState(1, "");
+            }
+        }else{
+            startGame.color = Color.WHITE;
+        }
+
+        if(mouseListener.getMouseX() > exitGame.x && mouseListener.getMouseX() < exitGame.x + exitGame.width &&
+                mouseListener.getMouseY() > exitGame.y - exitGame.height / 2.0 && mouseListener.getMouseY() < exitGame.y + exitGame.height / 2.0){
+            exitGame.color = new Color(158,158,158);
+            if(mouseListener.isMousePressed()){
+                Main.changeState(2, "");
+            }
+        }else{
+            exitGame.color = Color.WHITE;
+        }
+
+    }
+
+    public void draw(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
+        pong.draw(g2);
+        startGame.draw(g2);
+        exitGame.draw(g2);
+
+    }
+
+    public void stop(){
+        isRunning = false;
+    }
+
+    public void run() {
+        double lastFrameTime = 0.0;
+        double FPS = 60.0;
+        double frameTime = 1.0 / FPS;
+
+        while (isRunning) {
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+
+            if (deltaTime >= frameTime) {
+                lastFrameTime = time;
+                update(deltaTime);
+            }
+
+        }
+        this.dispose();
+        return;
+    }
 }
