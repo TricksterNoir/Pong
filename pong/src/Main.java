45d8c207edead19fd28d4d32cf1978f0ed1e3531
SylvioLabriola <sylvioez@gmail.com>
2024-10-22 17:55:04 -0300

    Update: Add a method to show the win/lose screen

--- pong/src/Main.java
+++ pong/src/Main.java
@@ -4,22 +4,24 @@ public class Main {
     public static Thread mainThread;
     public static MainMenu menu;
     public static Window window;
+    public static EndGameScreen endGameScreen;
+
     public static void main(String[] args) {
-        //Window window = new Window();
         menu = new MainMenu();
 
         mainThread = new Thread(menu);
         mainThread.start();
     }
 
-    public static void changeState(int newState){
+    public static void changeState(int newState, String message){
         if(newState == 1 && state == 0){
             menu.stop();
             window = new Window();
             mainThread = new Thread(window);
             mainThread.start();
-        }else if(newState == 0 && state == 1){
+        }else if(newState == 0 && state == 1 ||newState == 0 && state == 3){
             window.stop();
+            endGameScreen.stop();
             menu = new MainMenu();
             mainThread = new Thread(menu);
             mainThread.start();
@@ -28,6 +30,13 @@ public class Main {
                 window.stop();
             if(menu != null)
                 menu.stop();
+        }else if(newState == 3){
+            if(window != null){
+                window.stop();
+                endGameScreen = new EndGameScreen(message);
+                mainThread = new Thread(endGameScreen);
+                mainThread.start();
+            }
         }
         state = newState;
     }
