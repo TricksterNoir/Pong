f5225e72e9e431b187187e5135b4b62d99bb7556
SylvioLabriola <sylvioez@gmail.com>
2024-11-11 16:06:29 -0300

    change: created a round shape to the ball and the paddles

--- pong/src/Rect.java
+++ pong/src/Rect.java
@@ -1,2 +1,20 @@
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
 public class Rect {
+    public double x, y, width, height;
+    public Color color;
+
+    public Rect(double x, double y, double width, double height, Color color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    public void draw(Graphics2D g2){
+        g2.setColor(color);
+        g2.fill(new RoundRectangle2D.Double(x, y, width, height,15,15));
+    }
 }
