f69ee13b71b5586d4f5f4012176e6793ef037cf1
SylvioLabriola <sylvioez@gmail.com>
2024-11-11 13:51:41 -0300

    change: separated the controllers so the player and the AI can have their own and manipulate the AI difficulty control.

--- pong/src/AiController.java
+++ pong/src/AiController.java
@@ -1,2 +1,23 @@
 public class AiController {
+    public PlayerController playerController;
+    public Rect ball;
+    public Rect rect;
+
+    public AiController(PlayerController plrController, Rect ball, Rect rect) {
+        this.playerController = plrController;
+        this.ball = ball;
+        this.rect = rect;
+    }
+
+    public void update(double dt) {
+        playerController.update(dt);
+
+        if (ball.y < playerController.rect.y) {
+            if (rect.y - Constants.AI_PADDLE_SPEED * dt >= Constants.TOOLBAR_HEIGHT_TOP)
+                this.rect.y -= Constants.AI_PADDLE_SPEED * dt;
+        } else if (ball.y + ball.height > playerController.rect.y + playerController.rect.height) {
+            if ((rect.y + Constants.AI_PADDLE_SPEED * dt) + rect.height <= Constants.SCREEN_HEIGHT - Constants.TOOLBAR_HEIGHT_BOTTOM)
+                this.rect.y += Constants.AI_PADDLE_SPEED * dt;
+        }
+    }
 }
--- pong/src/PlayerController.java
+++ pong/src/PlayerController.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.awt.event.KeyEvent;
 
 public class PlayerController {
+    public Rect rect;
+    public KL keyListener;
+
+    public PlayerController(Rect rect, KL keyListener) {
+        this.rect = rect;
+        this.keyListener = keyListener;
+    }
+
+    public PlayerController(Rect rect) {
+        this.rect = rect;
+        this.keyListener = null;
+    }
+
+    public void update(double dt) {
+        if (keyListener != null) {
+            if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+                moveDown(dt);
+            } else if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+                moveUp(dt);
+            }
+        }
+    }
+
+    public void moveUp(double dt) {
+        if (rect.y - Constants.PL_PADDLE_SPEED * dt >= Constants.TOOLBAR_HEIGHT_TOP)
+            this.rect.y -= Constants.PL_PADDLE_SPEED * dt;
+    }
+
+    public void moveDown(double dt) {
+        if ((rect.y + Constants.PL_PADDLE_SPEED * dt) + rect.height <= Constants.SCREEN_HEIGHT - Constants.TOOLBAR_HEIGHT_BOTTOM)
+            this.rect.y += Constants.PL_PADDLE_SPEED * dt;
+    }
 }
