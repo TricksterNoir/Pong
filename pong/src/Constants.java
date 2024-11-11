8927a826e6ea38b70ee02f57f43e90ac285e8aa7
SylvioLabriola <sylvioez@gmail.com>
2024-11-11 13:40:29 -0300

    Add: Created the difficulty selector do change the AI paddle speed and separated the AI controller and the Player controller.

--- pong/src/Constants.java
+++ pong/src/Constants.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.awt.Color;
 
 public class Constants {
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
+    public static final String SCREEN_TITLE = "Pong, Ping Pong!";
+
+    public static final double PADDLE_WIDTH = 20;
+    public static final double PADDLE_HEIGHT = 80;
+    public static final Color PADDLE_COLOR  = Color.WHITE;
+
+    public static final double BALL_WIDTH = 10;
+    public static double BALL_SPEED = 250;
+
+    public static final double H2_PADDING = 40;
+    public static final double PL_PADDLE_SPEED = 180.0;
+    public static double AI_PADDLE_SPEED = 180.0;
+
+    public static double TOOLBAR_HEIGHT_TOP;
+    public static double TOOLBAR_HEIGHT_BOTTOM;
+
+    public static final double MAX_ANGLE = 45;
+
+    public static final int TEXT_Y_POS = 70;
+    public static final int TEXT_X_POS = 10;
+    public static final int TEXT_SIZE = 40;
+    public static final int TITLE_TEXT_SIZE = 60;
+
+    public static final int WIN_SCORE = 11;
+
+    public static final double EASY_SPEED = 120;
+    public static final double MEDIUM_SPEED = 150;
+    public static final double HARD_SPEED = 200;
 }
